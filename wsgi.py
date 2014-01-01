902512485b1f6936524ba05d8425a158598405f8
Phus Lu <phus.lu@gmail.com>
2014-01-01 01:03:55 -0800

    refine wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,21 +1,18 @@
 #!/usr/bin/env python
 # coding:utf-8
 
-from app import application
+import sys
+import os
+import logging
+import shlex
+import gunicorn.app.wsgiapp
 
-if __name__ == '__main__':
-    import os, logging
-    logging.basicConfig(level=logging.INFO, format='%(levelname)s - - %(asctime)s %(message)s', datefmt='[%b %d %H:%M:%S]')
-    host = os.environ.get('OPENSHIFT_DIY_IP', '127.0.0.1')
-    port = os.environ.get('OPENSHIFT_DIY_PORT', '8080')
-    logging.info('local python application serving at %s:%s', host, port)
-    from gunicorn.app.wsgiapp import WSGIApplication
-    class GunicornApplication(WSGIApplication):
-        def init(self, parser, opts, args):
-            return {'bind': '%s:%d' % (host, int(port)),
-                    'workers': 2,
-                    'worker_class': 'gevent'}
-        def load(self):
-            return application
-    GunicornApplication().run()
+host = os.environ.get('OPENSHIFT_DIY_IP', '127.0.0.1')
+port = os.environ.get('OPENSHIFT_DIY_PORT', '8080')
+
+cmd = 'gunicorn -b %s:%d -w 2 -k gevent app:application' % (host, int(port))
+sys.argv = shlex.split(cmd)
+logging.basicConfig(level=logging.INFO, format='%(levelname)s - - %(asctime)s %(message)s', datefmt='[%b %d %H:%M:%S]')
+os.chdir(os.path.abspath(os.path.dirname(__file__)))
+gunicorn.app.wsgiapp.run()
 
